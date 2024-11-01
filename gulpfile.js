c1afe9c5fd1e8374dc20b6928abfcf8bdbd649fb
MiguelVivar <miguelvivarfarfan@gmail.com>
2024-11-01 17:28:18 -0500

    Subiendo archivos4

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,34 @@
 const {src, dest, watch, series, parallel} = require('gulp');
+
+// CSS y SASS
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
+
+// Imagenes
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
+
+function versionWebp() {
+    return src('src/img/**/*.{png,jpg}')
+        .pipe(webp())
+        .pipe(dest('build/img'))
+};
+function versionAvif() {
+    const opciones = {
+        quality: 50
+    }
+    return src('src/img/**/*.{png,jpg}')
+        .pipe(avif(opciones))
+        .pipe(dest('build/img'))
+}
+
+function imagenes() {
+    return src('src/img/**/*')
+        .pipe( imagemin({ optimizationLevel: 3 }) )
+        .pipe( dest('build/img') )
+};
 
 function css(done) {
     // Compilar SASS
@@ -12,16 +39,20 @@ function css(done) {
         .pipe(dest('build/css'))
     done();
 };
-function dev() {
-    watch('src/scss/**/*.scss', css);
-}
 function tareaDefault() {
     console.log("Soy la teara por default");
-}
+};
+function dev() {
+    watch('src/scss/**/*.scss', css);
+    watch('src/img/**/*', imagenes)
+};
 
 exports.css = css;
 exports.dev = dev;
-exports.default = series(css, dev);
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.default = series(imagenes, versionWebp, versionAvif, css, dev);
 
 // Series - Se incia un tarea, hasta que finaliza y se inicia la siguiente
 // Parallel - Todas inician al mismo tiempo
